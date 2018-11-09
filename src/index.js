f30f92209e9bbe40090898a2b5f062f0df4efef5
Thiago Novato <thiagonovato@gmail.com>
2018-11-09 14:52:01 -0200

    Alterando configuração da porta do servidor.

--- src/index.js
+++ src/index.js
@@ -21,6 +21,8 @@ app.use(cors())
 app.use(express.json())
 app.use(require('./routes'))
 
-server.listen(3000, () => {
-    console.log(';) Servidor iniciado...')
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+    console.log(';) Servidor iniciado na porta ' + port)
 })
